20fa2b1de38ce8e2e1a26f094c1e662294079732
tin-juhee <juhee@tindlo.com>
2026-05-14 10:05:30 +0900

    feat: 아코디언 기본 요구사항
    - ui 기본
    - 헤더클릭하면 열리도록
    - 열린 아코디언 클릭하면 닫히도록
    - 다른 헤더 클릭하면 클릭한거 열리도록 원래열렸던건 닫힘

--- apps/accordion/src/App.tsx
+++ apps/accordion/src/App.tsx
@@ -4,8 +4,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
-      <h1 className="text-2xl font-bold text-green-400">Assignment</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-neutral-900 text-white">
+      <h1 className="text-2xl font-bold text-green-400">Interacion Lab</h1>
       <>
         <Accodion />
       </>
